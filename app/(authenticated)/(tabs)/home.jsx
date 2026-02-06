7bbc67d344b6ac6cb361f0242952d84c173dc336
Kamalveer singh <kamalhara7@gmail.com>
2026-02-06 21:37:44 +0530

    Flat list created

--- app/(authenticated)/(tabs)/home.jsx
+++ app/(authenticated)/(tabs)/home.jsx
@@ -1,17 +1,59 @@
-import { useRouter } from "expo-router";
-import { FlatList } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RideCard from "../../components/RideCard";
+import { icons, images } from "../../constants";
 import rides from "../../data/rides";
 
 export default function Home() {
-  const router = useRouter();
+  const { user } = useUser();
+  const isLoading = false;
+
+  const handleSignOut = () => {
+    // Implement sign-out logic here
+  };
+
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
         data={rides.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
-      ></FlatList>
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!isLoading ? (
+              <>
+                <Image source={images.noResult} className="h-40 w-40" />
+                <Text className="text-lg text-gray-500">
+                  No rides available
+                </Text>
+              </>
+            ) : (
+              <Text className="text-lg text-gray-500">Loading...</Text>
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-lg font-bold">
+                Welcome{"  "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}
+                👋🏻
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="flex flex-row items-center gap-x-1"
+              >
+                <Image source={icons.out} className="h-5 w-5" />
+              </TouchableOpacity>
+            </View>
+          </>
+        )}
+      />
     </SafeAreaView>
   );
 }
--- app/components/RideCard.jsx
+++ app/components/RideCard.jsx
@@ -1,4 +1,6 @@
 import { Image, Text, View } from "react-native";
+import { icons } from "../constants";
+import { formatDate, formatTime } from "../lib/utils";
 
 export default function RideCard({
   ride: {
@@ -12,11 +14,12 @@ export default function RideCard({
     destination_latitude,
     origin_longitude,
     origin_latitude,
+    payment_status,
   },
 }) {
   return (
     <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm mb-3 shadow-neutral-300">
-      <View className="flex flex-row items-center justify-between p-3">
+      <View className="flex flex-col items-center justify-center p-3">
         <View className="flex flex-row items-center justify-between">
           <Image
             source={{
@@ -24,9 +27,62 @@ export default function RideCard({
             }}
             className="w-[80px] h-[90px] rounded-lg"
           />
+
+          <View className="flex flex-col mx-5 gap-y-5 flex-1">
+            <View className="flex flex-row gap-x-2 items-center">
+              <Image source={icons.to} className="w-5 h-5" />
+              <Text className="text-md font-medium" numberOfLines={1}>
+                {origin_address}
+              </Text>
+            </View>
+
+            <View className="flex flex-row gap-x-2 items-center">
+              <Image source={icons.point} className="w-5 h-5" />
+              <Text className="text-md font-medium" numberOfLines={1}>
+                {destination_address}
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View className="flex items-center justify-center flex-col w-full mt-5 rounded-lg p-3 ">
+          <View className="flex flex-row items-center justify-between w-full mb-5">
+            <Text className="text-md text-gray-500 font-medium">
+              Date & Time
+            </Text>
+            <Text className="text-md text-gray-500 font-medium">
+              {formatDate(created_at)} {formatTime(ride_time)}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center justify-between w-full mb-5">
+            <Text className="text-md text-gray-500 font-medium">Driver</Text>
+            <Text className="text-md text-gray-500 font-medium">
+              {driver.first_name} {driver.last_name}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center justify-between w-full mb-5">
+            <Text className="text-md text-gray-500 font-medium">
+              Car Seats{" "}
+            </Text>
+            <Text className="text-md text-gray-500 font-medium">
+              {driver.car_seats}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center justify-between w-full mb-5">
+            <Text className="text-md text-gray-500 font-medium">
+              Payment Status
+            </Text>
+            <Text
+              className={`text-md font-medium ${payment_status === "Paid" ? "text-green-500" : "text-red-500"}`}
+            >
+              {payment_status}
+            </Text>
+          </View>
         </View>
       </View>
-      <Text className=" text-3xl">{driver.first_name}</Text>
     </View>
   );
 }
