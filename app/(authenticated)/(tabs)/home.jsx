74f2fdeca59ed56d04abb3ade999593505e20f8c
Kamalveer singh <kamalhara7@gmail.com>
2026-02-07 19:19:46 +0530

    flat list bug fixed

--- app/(authenticated)/(tabs)/home.jsx
+++ app/(authenticated)/(tabs)/home.jsx
@@ -8,7 +8,6 @@ import {
   Keyboard,
   Text,
   TouchableOpacity,
-  TouchableWithoutFeedback,
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -70,65 +69,64 @@ export default function Home() {
   }, []);
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <SafeAreaView className="bg-general-500 flex-1">
-        <FlatList
-          data={rides.slice(0, 5)}
-          renderItem={({ item }) => <RideCard ride={item} />}
-          className="px-5"
-          keyboardShouldPersistTaps="handled"
-          contentContainerStyle={{ paddingBottom: 100 }}
-          ListEmptyComponent={() => (
-            <View className="flex flex-col items-center justify-center">
-              {!isLoading ? (
-                <>
-                  <Image source={images.noResult} className="h-40 w-40" />
-                  <Text className="text-lg text-gray-500">
-                    No rides available
-                  </Text>
-                </>
-              ) : (
-                <Text className="text-lg text-gray-500">Loading...</Text>
-              )}
-            </View>
-          )}
-          ListHeaderComponent={() => (
-            <>
-              <View className="flex flex-row items-center justify-between my-5">
-                <Text className="text-lg font-bold">
-                  Welcome{"  "}
-                  {user?.firstName ||
-                    user?.emailAddresses[0].emailAddress.split("@")[0]}
-                  👋🏻
-                </Text>
-                <TouchableOpacity
-                  onPress={handleSignOut}
-                  className="flex flex-row items-center gap-x-1"
-                >
-                  <Image source={icons.out} className="h-5 w-5" />
-                </TouchableOpacity>
-              </View>
-
-              <OSMSearchBar
-                onSelect={handleDestinationSearch}
-                icon={icons.search}
-                ContainerStyle="bg-white shadow-md shadow-neutral-300 rounded-full py-1.5"
-                textInputBackgroundColor="white"
-              />
+    <SafeAreaView className="bg-general-500 flex-1">
+      <FlatList
+        data={rides.slice(0, 5)}
+        renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        onScrollBeginDrag={Keyboard.dismiss}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!isLoading ? (
               <>
-                <Text className="text-xl font-bold mt-5 mb-3">
-                  You&apos;re current location
+                <Image source={images.noResult} className="h-40 w-40" />
+                <Text className="text-lg text-gray-500">
+                  No rides available
                 </Text>
-
-                <View className="w-full h-[300px] overflow-hidden rounded-2xl">
-                  <Map />
-                </View>
               </>
-              <Text className="text-lg font-bold mt-5 mb-3">Recent Rides</Text>
+            ) : (
+              <Text className="text-lg text-gray-500">Loading...</Text>
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-lg font-bold">
+                Welcome{"  "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}
+                👋🏻
+              </Text>
+              <TouchableOpacity
+                onPress={handleSignOut}
+                className="flex flex-row items-center gap-x-1"
+              >
+                <Image source={icons.out} className="h-5 w-5" />
+              </TouchableOpacity>
+            </View>
+
+            <OSMSearchBar
+              onSelect={handleDestinationSearch}
+              icon={icons.search}
+              ContainerStyle="bg-white shadow-md shadow-neutral-300 rounded-full py-1.5"
+              textInputBackgroundColor="white"
+            />
+            <>
+              <Text className="text-xl font-bold mt-5 mb-3">
+                You&apos;re current location
+              </Text>
+
+              <View className="w-full h-[300px] overflow-hidden rounded-2xl">
+                <Map />
+              </View>
             </>
-          )}
-        />
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+            <Text className="text-lg font-bold mt-5 mb-3">Recent Rides</Text>
+          </>
+        )}
+      />
+    </SafeAreaView>
   );
 }
